92244b2c9293d06aabdf91ca386071714bd528aa
PalakurthiRahul01 <palakurthirahul01@gmail.com>
2026-04-14 10:44:18 +0530

    Enum class and Data

---
+++ Src/EnumClasses.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Enum_connection(Enum):
+    BAUD_RATE   = '115200'
+    NO_PORTS_FOUND ='No Ports Found'
+    NO_PORTS = 'No Ports'
+    DISCONNECT='Disconnect'
+    CONNECT = 'Connect'
+
+class Enum_Color_Background(Enum):
+    GREEN_COLOR  = '#00C853'
+    BACKGROUND_COLOR_GREEN = '"background-color: #00C853; color: white;"'
+    BACKGROUND_COLOR_NORMAL = '"background-color: #444; color: white;"'
+
+
+
+
+
